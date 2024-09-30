0be09992993a65c1b6fe5e070045ca3516332e39
Srijan Mitra <srijan.mtr@gmail.com>
2024-10-01 02:30:37 +0530

    Assignment3 completed

--- SELENIUM/src/Waits/Assignment3_SynchronizationWithExplicitWait.java
+++ SELENIUM/src/Waits/Assignment3_SynchronizationWithExplicitWait.java
@@ -1,9 +1,11 @@
 package Waits;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Wait;
@@ -15,22 +17,31 @@ public class Assignment3_SynchronizationWithExplicitWait {
 		// TODO Auto-generated method stub
 		String username = "rahulshettyacademy";
 		String password = "learning";
-		String[] items = {};
+		
 		
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://rahulshettyacademy.com/loginpagePractise/");
 		
+		
 		login(driver, username, password);
-//		checkOutProcess(driver, items);
+		checkOutProcess(driver);
 		
 		Thread.sleep(5000);
 		driver.quit();
 
 	}
 	
-	public static void checkOutProcess(WebDriver driver, String[] items) {
+	public static void checkOutProcess(WebDriver driver) {
+		Wait<WebDriver> w = new WebDriverWait(driver, Duration.ofSeconds(7));
+		w.until(ExpectedConditions.visibilityOfElementLocated(By.partialLinkText("Checkout")));
 		
+		List<WebElement> products = driver.findElements(By.xpath("//div[@class='card-footer']//button[@class='btn btn-info']"));
+		for(int i=0; i<products.size(); i++) {
+			products.get(i).click();
+		}
+		
+		driver.findElement(By.partialLinkText("Checkout")).click();
 	}
 	
 	public static void login(WebDriver driver, String username, String password) throws InterruptedException {
