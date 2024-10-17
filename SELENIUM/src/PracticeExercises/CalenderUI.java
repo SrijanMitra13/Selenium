085188bb07efe648d68af821d03da53a49bddc12
Srijan Mitra <srijan.mtr@gmail.com>
2024-10-17 13:07:31 +0530

    Calender UI Part 2

--- SELENIUM/src/PracticeExercises/CalenderUI.java
+++ SELENIUM/src/PracticeExercises/CalenderUI.java
@@ -1,11 +1,14 @@
 package PracticeExercises;
 
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class CalenderUI {
 
@@ -14,6 +17,7 @@ public class CalenderUI {
 		String monthNumber = "6";
 		String date = "15";
 		String year = "2027";
+		String[] expectedList = {monthNumber, date, year};
 		
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
@@ -45,6 +49,13 @@ public class CalenderUI {
 		
 		// CLicking on the date - 15 in this case
 		driver.findElement(By.xpath("//abbr[contains(text(),'"+date+"')]")).click();
+		
+		//Getting the text out of the selected date
+		List<WebElement> actualList = driver.findElements(By.cssSelector(".react-date-picker__inputGroup__input"));
+		for(int i=0; i<actualList.size(); i++) {
+			System.out.println(actualList.get(i).getAttribute("value"));
+			Assert.assertEquals(actualList.get(i).getAttribute("value"), expectedList[i]);
+		}
 		
 		Thread.sleep(5000);
 		driver.quit();
