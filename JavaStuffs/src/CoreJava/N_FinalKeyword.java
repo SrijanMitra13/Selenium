922b724cf608b24ce45b0540c200d2e5889d8a4b
Srijan Mitra <srijan.mtr@gmail.com>
2024-10-28 14:01:18 +0530

    Java Part 2: Access Modifiers

--- JavaStuffs/src/CoreJava/N_FinalKeyword.java
+++ JavaStuffs/src/CoreJava/N_FinalKeyword.java
@@ -1,10 +1,16 @@
 package CoreJava;
 
 public class N_FinalKeyword {
+	
+	//Class name as final that means you cannot extend that class
+	final void getData() {
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		final int i=4; // Using finalmakes a variable immutable
+//		i=5;
 	}
 
 }
---
+++ JavaStuffs/src/JavaPractice/HelloJava.java
@@ -0,0 +1,28 @@
+package JavaPractice;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		byte b = 127;
+		int a =260;
+		byte k =(byte) a;
+		
+		System.out.println(k);
+		
+		
+		
+		float f = 5.6f;
+		int t = (int) f;
+		
+		System.out.println(t);
+		
+		
+		
+		byte c = 10;
+		byte b = 30;
+		int d = c*b;
+		System.out.println(d);
+	}
+
+}
