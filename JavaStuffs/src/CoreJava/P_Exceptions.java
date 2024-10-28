179311bce98eac7977f0da5750b8e937f3fa1210
Srijan Mitra <srijan.mtr@gmail.com>
2024-10-29 01:59:07 +0530

    Java Part 2: Try Catch

---
+++ JavaStuffs/src/CoreJava/P_Exceptions.java
@@ -0,0 +1,43 @@
+package CoreJava;
+
+public class P_Exceptions {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int b = 7;
+		int c = 0;
+		
+		try {
+			int k = b/c;
+			System.out.println(k);
+		} catch(Exception e) {
+			System.out.println("I catched the exception");
+		}
+		
+		tryCatch();
+	}
+	
+	
+	//One try can be followed by multiple catch blocks
+	//Catch should be an immediate block after try
+	public static void tryCatch() {
+		int b = 7;
+		int c = 0;
+		
+		try {
+//			int k = b/c;
+			
+			int arr[] = new int[5];
+			
+			System.out.println(arr[7]);
+		} catch(ArithmeticException et) {
+			System.out.println("I catched the Arithmetic exception");
+		} catch(IndexOutOfBoundsException ets) {
+			System.out.println("I catched the IndexOutOfBoundExceptions exception");
+		} catch(Exception e) {
+			System.out.println("I catched the exception");
+		}
+	}
+
+}
