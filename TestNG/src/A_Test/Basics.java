c82ba524c2c39170a2ae2d6006d1eedd64f9638b
Srijan Mitra <srijan.mtr@gmail.com>
2024-11-01 02:52:52 +0530

    TestNG

---
+++ TestNG/src/A_Test/Basics.java
@@ -0,0 +1,16 @@
+package A_Test;
+
+import org.testng.annotations.Test;
+
+public class Basics {
+	
+	@Test
+	public void demo() {
+		System.out.println("Hello");
+	}
+	
+	@Test
+	public void secondTest() {
+		System.out.println("Bye");
+	}
+}
---
+++ TestNG/src/A_Test/Basics2.java
@@ -0,0 +1,12 @@
+package A_Test;
+
+import org.testng.annotations.Test;
+
+public class Basics2 {
+
+	@Test
+	public void ploan() {
+		System.out.println("Good");
+	}
+
+}
