5c3ea2e48bf86a57df828cfcdcae77b2c87aac93
Srijan Mitra <srijan.mtr@gmail.com>
2024-11-02 13:13:12 +0530

    testng.xml

---
+++ TestNG/src/A_Test/Basics3.java
@@ -0,0 +1,23 @@
+package A_Test;
+
+import org.testng.annotations.Test;
+
+public class Basics3 {
+	@Test
+	public void webLoginCarLoan() {
+		//selenium
+		System.out.println("weblogincar");
+	}
+	
+	@Test
+	public void mobileLoginCarLoan() {
+		//Appium
+		System.out.println("Mobilelogincar");
+	}
+	
+	@Test
+	public void LoginAPICarLoan() {
+		//Rest API automation
+		System.out.println("APIlogincar");
+	}
+}
---
+++ TestNG/src/A_Test/Basics4.java
@@ -0,0 +1,23 @@
+package A_Test;
+
+import org.testng.annotations.Test;
+
+public class Basics4 {
+	@Test
+	public void webLoginHomeLoan() {
+		//selenium
+		System.out.println("webloginhome");
+	}
+	
+	@Test
+	public void mobileLoginCarLoan() {
+		//Appium
+		System.out.println("Mobileloginhome");
+	}
+	
+	@Test
+	public void LoginAPICarLoan() {
+		//Rest API automation
+		System.out.println("APIloginhome");
+	}
+}
