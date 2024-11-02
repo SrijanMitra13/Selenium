badbcc2a6075b30dce15440205dfb64d6def883f
Srijan Mitra <srijan.mtr@gmail.com>
2024-11-02 13:41:30 +0530

    testng2.xml

--- TestNG/src/A_Test/Basics3.java
+++ TestNG/src/A_Test/Basics3.java
@@ -12,11 +12,23 @@ public class Basics3 {
 	@Test
 	public void mobileLoginCarLoan() {
 		//Appium
-		System.out.println("Mobilelogincar");
+		System.out.println("Mobile login car");
 	}
 	
 	@Test
-	public void LoginAPICarLoan() {
+	public void mobileSignInCarLoan() {
+		//Appium
+		System.out.println("Mobile SignIn car");
+	}
+	
+	@Test
+	public void mobileSignOutCarLoan() {
+		//Appium
+		System.out.println("Mobile SignOut car");
+	}
+	
+	@Test
+	public void APICarLoan() {
 		//Rest API automation
 		System.out.println("APIlogincar");
 	}
