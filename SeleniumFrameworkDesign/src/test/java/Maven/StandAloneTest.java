018090232e69929b756b006443b96824807c0a66
Srijan Mitra <srijan.mtr@gmail.com>
2024-11-14 00:42:10 +0530

    Maven

--- SeleniumFrameworkDesign/src/test/java/Maven/StandAloneTest.java
+++ SeleniumFrameworkDesign/src/test/java/Maven/StandAloneTest.java
@@ -1,9 +1,14 @@
 package Maven;
 
 import java.time.Duration;
+import java.util.List;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -17,7 +22,27 @@ public class StandAloneTest {
 		driver.manage().window().maximize();
 		driver.get("https://rahulshettyacademy.com/client/");
 		
-		Thread.sleep(3000);
+		driver.findElement(By.id("userEmail")).sendKeys("john.marston@gmail.com");
+		driver.findElement(By.id("userPassword")).sendKeys("Srijan10");
+		driver.findElement(By.id("login")).click();
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".mb-3")));
+		
+		List<WebElement> products = driver.findElements(By.cssSelector(".mb-3"));
+		
+		WebElement prod = products.stream().filter(product->
+		product.findElement(By.cssSelector("b")).getText().equals("ZARA COAT 3")).findFirst().orElse(null);
+		
+		prod.findElement(By.cssSelector(".card-body button:last-of-type")).click();
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#toast-container")));
+		wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.cssSelector(".ng-animating"))));
+//		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector(".ng-animating")));
+		
+		driver.findElement(By.cssSelector("[routerlink*='cart']")).click();
+		
+		Thread.sleep(10000);
 		driver.quit();
 	}
 
