116222a86985f5eed37d01f3e3d63b5dfbf8b570
Srijan Mitra <srijan.mtr@gmail.com>
2024-11-13 23:10:45 +0530

    Maven

---
+++ SeleniumFrameworkDesign/src/test/java/Maven/StandAloneTest.java
@@ -0,0 +1,24 @@
+package Maven;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class StandAloneTest {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/client/");
+		
+		Thread.sleep(3000);
+		driver.quit();
+	}
+
+}
