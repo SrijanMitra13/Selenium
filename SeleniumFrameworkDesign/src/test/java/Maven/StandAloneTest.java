d6e4a18c2749100b2554d8337cec00caf8627353
Srijan Mitra <srijan.mtr@gmail.com>
2024-11-17 01:14:36 +0530

    Maven

--- SeleniumFrameworkDesign/src/test/java/Maven/StandAloneTest.java
+++ SeleniumFrameworkDesign/src/test/java/Maven/StandAloneTest.java
@@ -7,8 +7,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -16,6 +18,8 @@ public class StandAloneTest {
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
+		
+		String productName = "ZARA COAT 3";
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
@@ -41,6 +45,26 @@ public class StandAloneTest {
 //		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector(".ng-animating")));
 		
 		driver.findElement(By.cssSelector("[routerlink*='cart']")).click();
+		
+		List<WebElement> cartProducts = driver.findElements(By.cssSelector(".cartSection h3"));
+		
+		boolean match = cartProducts.stream().anyMatch(cartProduct-> cartProduct.getText()
+						.equalsIgnoreCase(productName));
+		Assert.assertTrue(match);
+		
+		driver.findElement(By.cssSelector(".totalRow button")).click();
+		
+		Actions a = new Actions(driver);
+		a.sendKeys(driver.findElement(By.cssSelector("[placeholder='Select Country']")), "india").build().perform();
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".ta-results")));
+		
+		driver.findElement(By.xpath("(//button[contains(@class,'ta-item')])[2]")).click();
+		driver.findElement(By.cssSelector(".action__submit")).click();
+		
+		String confirmMessage = driver.findElement(By.cssSelector(".hero-primary")).getText();
+		Assert.assertTrue(confirmMessage.equalsIgnoreCase("THANKYOU FOR THE ORDER."));
+		driver.close();
 		
 		Thread.sleep(10000);
 		driver.quit();
